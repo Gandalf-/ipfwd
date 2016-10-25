6b4ab347ab72df250dea306fc3cdb32999b032a4
Gandalf- <austin.voecks@gmail.com>
2016-10-25 22:52:24 +0000

    enhance loadgen

--- loadgen/loadgen.c
+++ loadgen/loadgen.c
@@ -1,27 +1,68 @@
 #include <math.h>
 #include <time.h>
 #include <stdio.h>
-
-const int ten_to_fifteen = 50000;
+#include <stdlib.h>
 
 int main(int argc, char **argv){
 
-  float x = 0, y = 0, z = 0;
-
+  int loads[100] = {0};
+  int load, i;
+  double elapsed, duration;
+  float x, y, z;
+  time_t start, end;
   struct timespec tim, tim2;
-  tim.tv_sec = 0;
-  tim.tv_nsec = 5000;
 
-  while (1) {
-    if (x < 1 || y < 1 || z < 1) {
-      x = 0xffffffff;
-      y = 0xffffffff;
-      z = 0xffffffff;
+  /* supported loads */
+  loads[15] = 30000;
+  loads[25] = 20000;
+  loads[50] = 10000;
+  loads[75] = 8500;
+
+  if (argc != 3) {
+    printf("usage: %s [cpu load %%] [duration]\n", argv[0]);
+    return -1;
+  }
+
+  /* get and verify parameters */
+  load = atoi(argv[1]);
+  duration = (double)atoi(argv[2]);
+
+  if(load < 0 || load > 100 || duration < 1 ) {
+    printf("usage: %s [cpu load %%] [duration]\n", argv[0]);
+    return -1;
+  }
+
+  if (loads[load] == 0) {
+    printf("supported percentages: ");
+    for (i = 0; i < 100; i++)
+      if (loads[i] != 0)
+        printf("%d, ", i);
+    printf("\n");
+    return -1;
+  }
+
+  /* pre loop setup */
+  tim.tv_sec = 0;
+  tim.tv_nsec = loads[load];
+  time(&start);
+  printf("starting %d (%d) percent load for %d seconds\n", 
+      load, loads[load], (int)duration);
+
+  while (elapsed < duration) {
+
+    /* work */
+    if (x < 1 || y < 1 || z < 1)
+      x = y = z = 0xffffffff;
+
+    for (int i = 0; i < 10; i++) {
+      x = sqrt(x); x = sqrt(x); x = sqrt(x);
+      y = sqrt(y); y = sqrt(y); y = sqrt(y);
+      z = sqrt(z); z = sqrt(z); z = sqrt(z);
     }
 
-    x = sqrt(x);
-    y = sqrt(y);
-    z = sqrt(z);
+    /* check times */
+    time(&end);
+    elapsed = difftime(end, start);
 
     if(nanosleep(&tim , &tim2) < 0 )
       return -1;
