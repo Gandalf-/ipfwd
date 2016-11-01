b6b71ae21052aa32ddada6a9bfff4783cf7b62ee
Gandalf- <austin.voecks@gmail.com>
2016-11-01 23:00:41 +0000

    tweaks for loadgen

--- loadgen/loadgen.c
+++ loadgen/loadgen.c
@@ -15,7 +15,7 @@ int main(int argc, char **argv){
   /* supported loads */
   loads[15] = 30000;
   loads[25] = 20000;
-  loads[50] = 10000;
+  loads[50] = 9200;
   loads[75] = 8500;
   loads[95] = 7800;
 
