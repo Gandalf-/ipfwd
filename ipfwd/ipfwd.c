7b1753ca619d8fa0a9b68cf02b030bbe9fa1e0e4
Gandalf- <austin.voecks@gmail.com>
2016-11-08 13:30:12 -0800

    ipfwd gets system load through uptime()

--- ipfwd/ipfwd.c
+++ ipfwd/ipfwd.c
@@ -1,14 +1,39 @@
+#include <math.h>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#define check(ret, string) if (! ret) {error = string; goto ERROR;}
+const int hist_size = 5;
 
 int main(int argc, char **argv) {
 
-  double loads[3];
-  int ret = getloadavg(loads, 3);
+  int i;
+  long num_cpus;
+  double slope, previous;
+  double loads[1];
+  double history[hist_size];
+  char *error = "unknown error";
 
-  for (int i = 0; i < 3; i++) {
-    printf("%d : %f\n", i, loads[i]);
+  num_cpus = sysconf(_SC_NPROCESSORS_ONLN);
+
+  while (1) {
+    check(getloadavg(loads, 1), "could not get load average");
+
+    for (i = hist_size; i > 0; i--)
+      history[i] = history[i - 1];
+    history[0] = loads[0] / num_cpus;
+
+    slope = history[0] - history[1];
+
+    if (slope != 0)
+      printf("current: %f, slope: %f\n", 
+          history[0], slope);
+    sleep(1);
   }
 
+ERROR:
+  printf("error: %s\n", error);
   return 0;
 }
