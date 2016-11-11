9ffeae2688bbfdb43e5ba248767a2ce218d22e7d
Gandalf- <austin.voecks@gmail.com>
2016-11-10 22:36:33 -0800

    Notes on new workstations, refactoring on ipfwd.c

--- ipfwd/ipfwd.c
+++ ipfwd/ipfwd.c
@@ -1,20 +1,23 @@
-#include <math.h>
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-#define check(ret, string) if (! ret) {error = string; goto ERROR;}
+#define check(ret, string) if (! ret) {errormsg = string; goto ERROR;}
+
 const int hist_size = 5;
 
 int main(int argc, char **argv) {
+  /* 
+    monitors system load using uptime()
+    normalizes results by number of cpus
+  */
 
   int i;
   long num_cpus;
-  double slope, previous;
-  double loads[1];
-  double history[hist_size];
-  char *error = "unknown error";
+  double slope, previous, prediction, error;
+  double loads[1], history[hist_size];
+  char *errormsg = "unknown error";
 
   num_cpus = sysconf(_SC_NPROCESSORS_ONLN);
 
@@ -25,15 +28,18 @@ int main(int argc, char **argv) {
       history[i] = history[i - 1];
     history[0] = loads[0] / num_cpus;
 
+    error = abs((history[0] - prediction) * 100);
     slope = history[0] - history[1];
+    prediction = history[0] + slope;
 
     if (slope != 0)
-      printf("current: %f, slope: %f\n", 
-          history[0], slope);
+      printf(
+          "current: % 02.2f%%, prediction error: % 02.1f%%\n", 
+          history[0] * 100, error);
     sleep(1);
   }
 
 ERROR:
-  printf("error: %s\n", error);
+  printf("error: %s\n", errormsg);
   return 0;
 }
