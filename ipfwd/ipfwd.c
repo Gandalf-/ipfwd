d0203071ce0b6501ba7170e35e836517c8b146b8
Gandalf- <austin.voecks@gmail.com>
2016-11-15 22:21:59 -0800

    Start code ipfwd to update first rules

--- ipfwd/ipfwd.c
+++ ipfwd/ipfwd.c
@@ -2,23 +2,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-#define check(ret, string) if (! ret) {errormsg = string; goto ERROR;}
+#define check(ret, string) if (! ret) { fprintf(stderr, "error: %s\n", string); return -1;}
 
 const int hist_size = 5;
+const char *path = "/bin/echo";
+
+static char *errormsg = "unknown error";
+static char *get_rule_one[] = { "ipfw", "show 1", NULL};
+static char *del_rule_one[] = { "ipfw", "delete 1", NULL};
+static char *set_rule_one[] = {
+  "ipfw", "add 1 prob 1.000 allow ip any to any", NULL};
+
+double set_probability(double probability) {
+  /*
+    actually set the new first rule probability
+  */
+  pid_t pid;
+
+  if ((pid = fork()) == 0) {
+    execv(path, del_rule_one);
+    exit(1); 
+  }
+  else {
+    waitpid(pid, &status, 0);
+    check(!status, "failed deleting old rule"); 
+  }
+}
+
+double update_ipfw(double current, double probability) {
+  /* 
+    update the first rule depending on the current load
+  */
+  int status;
+  double baseline = 40.0;
+
+  /* do nothing if we're below 40% utilization */
+  if (current - baseline < 0) 
+    return probability;
+
+  return 0;
+}
 
 int main(int argc, char **argv) {
   /* 
-    monitors system load using uptime()
-    normalizes results by number of cpus
-  */
+     monitors system load using uptime()
+     normalizes results by number of cpus
+     */
 
-  int i;
+  int status, i;
   long num_cpus;
-  double slope, previous, prediction, error;
+  double slope, previous, prediction, error, probability;
   double loads[1], history[hist_size];
-  char *errormsg = "unknown error";
 
+  probability = 0.0;
   num_cpus = sysconf(_SC_NPROCESSORS_ONLN);
 
   while (1) {
@@ -26,20 +65,23 @@ int main(int argc, char **argv) {
 
     for (i = hist_size; i > 0; i--)
       history[i] = history[i - 1];
-    history[0] = loads[0] / num_cpus;
+    history[0] = loads[0] / num_cpus * 100;
 
     error = abs((history[0] - prediction) * 100);
     slope = history[0] - history[1];
     prediction = history[0] + slope;
 
-    if (slope != 0)
+    if (slope != 0) {
+      probability = update_ipfw(history[0], probability);
       printf(
-          "current: % 02.2f%%, prediction error: % 02.1f%%\n", 
-          history[0] * 100, error);
+          "current: % 02.2f%%, " 
+          "prediction error: % 02.1f%%, "
+          "probability: % 02.1f%%\n",
+          history[0],
+          error, 
+          probability);
+    }
     sleep(1);
   }
-
-ERROR:
-  printf("error: %s\n", errormsg);
   return 0;
 }
